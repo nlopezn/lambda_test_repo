398112378f3b7404c99162596ad2f5cb7a1b0f54
nlopezn <nlopez.n@gmail.com>
2022-05-06 09:18:04 -0700

    Update test-lambda.py

--- test-lambda.py
+++ test-lambda.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda ... this is an update ')
+        'body': json.dumps('Hello from Lambda ... Updating With Specific File dir Trigger ')
     }
