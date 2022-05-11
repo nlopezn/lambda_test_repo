aa97e3eb5bae1eebbd5a8776d90652a98135427a
nlopezn <nlopez.n@gmail.com>
2022-05-10 21:36:23 -0700

    Update codebuild.tf

--- pipeline/codebuild.tf
+++ pipeline/codebuild.tf
@@ -1,20 +1,4 @@
 
-#data "template_file" "buildspec_plan" {
-#  template = "${file("buildspec_plan.yaml")}"
-#  vars = {
-#    env          = var.env_name,
-#    TF_VERSION   = var.tf_version
-#  }
-#}
-
-#data "template_file" "buildspec_apply" {
-#  template = "${file("buildspec_apply.yaml")}"
-#  vars = {
-#    env          = var.env_name,
-#    TF_VERSION   = var.tf_version
-#  }
-#}
-
 resource "aws_iam_role" "codebuild-role" {
   name = "codebuild-role"
 
@@ -37,42 +21,28 @@ EOF
 resource "aws_iam_role_policy" "codebuild-role-policy" {
   role = aws_iam_role.codebuild-role.name
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Resource": [
-        "*"
-      ],
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": "*",
-      "Resource": "*"
-    }
-  ]
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Resource" : [
+          "*"
+        ],
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "*",
+        "Resource" : "*"
+      }
+    ]
+  })
 }
-POLICY
-}
-#
-#,
-#    {
-#      "Effect": "Allow",
-#      "Action": "secretsmanager:GetSecretValue",
-#      "Resource": "arn:aws:secretsmanager:us-east-1:390854006253:secret:siesa-prd-launch-resources-Ux4eJx"
-#    },
-#    {
-#      "Effect": "Allow",
-#      "Action": "kms:Decrypt",
-#      "Resource": "arn:aws:kms:us-east-1:390854006253:key/95e62567-312f-483f-8cfd-7808308ae22e"
-#    }
 
 resource "aws_codebuild_project" "terraform_plan_codebuild" {
   name          = var.codebuild_project_name_plan
@@ -95,32 +65,29 @@ resource "aws_codebuild_project" "terraform_plan_codebuild" {
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
 
-  }
-
-  environment_variable {
-      name  = "CODEBUILD_SRC_DIR"
+    environment_variable {
+      name  = "SRC_DIR"
       value = var.codebuild_src_dir
     }
 
+    environment_variable {
+        name  = "TF_VERSION"
+        value = var.tf_version
+    }
+  }
+
   logs_config {
     cloudwatch_logs {
-      group_name  = "log-group"
-      stream_name = "log-stream"
+      group_name  = "${var.org_identifier}-${var.environment}-codebuild-logs"
     }
-
-#    s3_logs {
-#      status   = "ENABLED"
-#      location = "${aws_s3_bucket.codepipeline_bucket.id}/build-log"
-#    }
   }
 
   source_version = "main"
 
   source {
-    type            = "GITHUB"
-    location        = "https://github.com/nlopezn/lambda_test_repo.git"
-    buildspec       = "buildspec_plan.yml"
-    git_clone_depth = 1
+    type            = "CODEPIPELINE"
+    buildspec       = var.buildspec_plan_yml
+    git_clone_depth = 0
   }
 
   tags = {
@@ -133,7 +100,7 @@ resource "aws_codebuild_project" "terraform_apply_codebuild" {
   name          = var.codebuild_project_name_apply
   description   = var.codebuild_project_name_apply
   build_timeout = "20"
-  service_role  = aws_iam_role.codebuild-role
+  service_role  = aws_iam_role.codebuild-role.arn
 
   artifacts {
     type = "CODEPIPELINE"
@@ -157,58 +124,17 @@ resource "aws_codebuild_project" "terraform_apply_codebuild" {
       group_name  = "log-group"
       stream_name = "log-stream"
     }
-
-#    s3_logs {
-#      status   = "ENABLED"
-#      location = "${aws_s3_bucket.codepipeline_bucket.id}/build-log"
-#    }
   }
 
   source_version = "main"
 
   source {
-    type            = "GITHUB"
-    location        = "https://github.com/nlopezn/lambda_test_repo.git"
-    buildspec       = "buildspec_apply.yml"
-    git_clone_depth = 1
+    type            = "CODEPIPELINE"
+    buildspec       = var.buildspec_plan_yml
+    git_clone_depth = 0
   }
-
-#  source {
-#    buildspec           = data.template_file.buildspec_apply.rendered
-#    git_clone_depth     = 0
-#    insecure_ssl        = false
-#    report_build_status = false
-#    type                = "CODEPIPELINE"
-#  }
-
+  
   tags = {
     name                = "${var.org_identifier}-${var.environment}-codebuild"
   }
-}
-
-# Webhook to repository
-resource "aws_codebuild_webhook" "webhook_to_repository" {
-  project_name = aws_codebuild_project.github-to-lambda.name
-  build_type   = "BUILD"
-  filter_group {
-    filter {
-      pattern = "*.tf"
-      type    = "FILE_PATH"
-    }
-    filter {
-      type    = "EVENT"
-      pattern = "PUSH"
-    }
-    filter {
-      type    = "HEAD_REF"
-      pattern = "main"
-    }
-  }
-}
-
-# Repository Authorization
-resource "aws_codebuild_source_credential" "source_auth" {
-  auth_type   = var.authorization_type
-  server_type = var.source_provider
-  token       = var.github_acc_token
 }
